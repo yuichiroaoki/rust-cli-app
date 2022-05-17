6d0974a7b46435e8999660038e774c37e1adc148
yuichiroaoki <yuichiroaokigcpproject@gmail.com>
2022-05-17 11:54:08 +0900

    request and env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use dotenv::dotenv;
+
+mod request;
+mod utils;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Show hello message
+    #[clap(long)]
+    hello: bool,
+
+    /// Show env
+    #[clap(long)]
+    env: bool,
+
+    /// Send get request
+    #[clap(long)]
+    request: bool,
+}
+
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let args = Args::parse();
+
+    if args.hello {
+        println!("Hello, world!");
+    }
+
+    if args.env {
+        let key = "MY_ENV_VAR";
+        let val = utils::get_env(key);
+        println!("{} = {}", key, val);
+    }
+
+    if args.request {
+        let url = "https://google.com";
+        let res = request::send_get_request(url).await;
+        assert_eq!(res.is_ok(), true);
+    }
+
 }
